8763569e8acb56cfea36b2158f6b34e3acae1415
Jailton Santos <jailtonmatos200@gmail.com>
2026-02-08 17:48:25 -0300

    feat: creation of REST endpoints and system initialization

---
+++ src/main/java/fridge/fridgeSistem/FridgeSistemApplication.java
@@ -0,0 +1,13 @@
+package fridge.fridgeSistem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FridgeSistemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FridgeSistemApplication.class, args);
+	}
+
+}
